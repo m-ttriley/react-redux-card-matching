9f7f30beab73c7ba90c75a7ba64ade3e50429db7
Matthew Riley <matthew.vincent.riley@gmail.com>
2018-08-26 23:30:52 -0400

    Adds Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './styles.scss'
+
+const Header = () => (
+  <div className={styles.header}>
+    <h1>NYT Games Code Test</h1>
+    <h2>By Matthew Riley</h2>
+  </div>
+)
+export default Header
