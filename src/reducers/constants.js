e4c5e02057f6feb35f442734931a04d8aa09ec2e
Matthew Riley <matthew.vincent.riley@gmail.com>
2018-08-26 21:46:50 -0400

    Adds constant for initial state - This is to give an outline of what the shape of the state will look like

---
+++ src/reducers/constants.js
@@ -0,0 +1,13 @@
+export const initialState = {
+  time: 0,
+  levels: [],
+  moves: 0,
+  currentLevel: '',
+  cards: {},
+  loading: false,
+  error: null,
+  matchedCards: [],
+  finished: false,
+  cardsFlipped: 0,
+  maxCardsFlipped: 2,
+}
